4f27af3082c5478abe91ff99e3e1d69bf1fc07b4
Shubham Lal <lal.shubham027@gmail.com>
2024-09-25 10:41:56 +0530

    modify menu anim

--- js/menu.js
+++ js/menu.js
@@ -1,41 +1,3 @@
-// MENU
-
-tl = new TimelineMax({ paused: true });
-
-tl.to(".menu-left", 1, {
-    left: 0,
-    ease: Expo.easeInOut
-});
-
-tl.to(".menu-right", 1, {
-    right: 0,
-    ease: Expo.easeInOut
-}, "-=1");
-
-tl.staggerFrom(".menu-links > div", 0.8, {
-    y: 100,
-    opacity: 0,
-    ease: Expo.easeOut
-}, "0.1", "-=0.4");
-
-tl.staggerFrom(".mail > div, .socials > div", 0.8, {
-    y: 100,
-    opacity: 0,
-    ease: Expo.easeOut
-}, "0.1", "-=1");
-
-tl.reverse();
-
-$(document).on("click", ".menu-open", function () {
-    tl.reversed(!tl.reversed());
-    $("body").css("overflow", "hidden");
-});
-
-$(document).on("click", ".menu-close", function () {
-    tl.reversed(!tl.reversed());
-    $("body").css("overflow", "auto");
-});
-
 // LOGO
 
 ScrollTrigger.create({
@@ -74,4 +36,42 @@ ScrollTrigger.create({
     end: "bottom 80%",
     scrub: true,
     markers: false
+});
+
+// MENU
+
+tl = gsap.timeline()
+
+tl.to(".menu-left", 1, {
+    left: 0,
+    ease: Expo.easeInOut
+});
+
+tl.to(".menu-right", 1, {
+    right: 0,
+    ease: Expo.easeInOut
+}, "-=1");
+
+tl.staggerFrom(".menu-links > div", 0.8, {
+    y: 100,
+    opacity: 0,
+    ease: Expo.easeOut
+}, "0.1", "-=0.4");
+
+tl.staggerFrom(".mail > div, .socials > div", 0.8, {
+    y: 100,
+    opacity: 0,
+    ease: Expo.easeOut
+}, "0.1", "-=1");
+
+tl.reverse();
+
+$(document).on("click", ".menu-open", function () {
+    tl.reversed(!tl.reversed());
+    $("body").css("overflow", "hidden");
+});
+
+$(document).on("click", ".menu-close", function () {
+    tl.reversed(!tl.reversed());
+    $("body").css("overflow", "auto");
 });
