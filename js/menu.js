42a9801f4ec59f50c585fa0eb3d91841b614c644
Shubham Lal <lal.shubham027@gmail.com>
2024-10-01 22:35:02 +0530

    update menu anim

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,22 @@
 const tl = gsap.timeline({ paused: true });
 
-tl.to(".menu-left", 1, {
+tl.to(".menu-left", 0.75, {
     left: 0,
     ease: Expo.easeInOut
 });
 
-tl.to(".menu-right", 1, {
+tl.to(".menu-right", 0.75, {
     right: 0,
     ease: Expo.easeInOut
-}, "-=1");
+}, "-=0.75");
 
-tl.staggerFrom(".menu-links > div", 0.8, {
+tl.staggerFrom(".menu-links > div", 0.5, {
     y: 100,
     opacity: 0,
     ease: Expo.easeOut
 }, "0.1", "-=0.4");
 
-tl.staggerFrom(".mail > div, .socials > div", 0.8, {
+tl.staggerFrom(".mail > div, .socials > div", 0.5, {
     y: 100,
     opacity: 0,
     ease: Expo.easeOut
--- js/page.js
+++ js/page.js
@@ -4,7 +4,7 @@ barba.init({
         enter(data) {
             gsap.from(data.next.container, {
                 opacity: 0,
-                duration: 1.5,
+                duration: 1,
                 onComplete: () => {
                     document.querySelector("main").style.opacity = 1;
                     document.querySelector("main").style.zIndex = 0;
