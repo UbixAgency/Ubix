dbdf35ff5054da1e5b353c4bf9776094a21d6b44
Shubham Lal <lal.shubham027@gmail.com>
2024-10-16 10:23:04 +0530

    alter home & about bg

--- js/about.js
+++ js/about.js
@@ -1,10 +1,53 @@
 function initAboutPage() {
-    const container = document.querySelector("#tiles"),
-        tile = document.querySelector(".tile");
+    const wrapper = document.getElementById("tiles");
 
-    for (let i = 0; i < 1599; i++) {
-        container.appendChild(tile.cloneNode());
+    let columns = 0, rows = 0, toggled = false;
+
+    const toggle = () => {
+        toggled = !toggled;
+        document.querySelector("main[data-barba-namespace='about']").classList.toggle("toggled");
     }
+
+    const handleOnClick = index => {
+        toggle();
+        anime({
+            targets: ".tile",
+            opacity: toggled ? 0 : 1,
+            delay: anime.stagger(50, {
+                grid: [columns, rows],
+                from: index
+            })
+        });
+    }
+
+    const createTile = index => {
+        const tile = document.createElement("div");
+        tile.classList.add("tile");
+        tile.style.opacity = toggled ? 0 : 1;
+        tile.onclick = e => handleOnClick(index);
+        return tile;
+    }
+
+    const createTiles = quantity => {
+        Array.from(Array(quantity)).map((tile, index) => {
+            wrapper.appendChild(createTile(index));
+        });
+    }
+
+    const createGrid = () => {
+        wrapper.innerHTML = "";
+        const size = document.body.clientWidth > 800 ? 100 : 50;
+        columns = Math.floor(document.body.clientWidth / size);
+        rows = Math.floor(window.innerHeight / size);
+
+        wrapper.style.setProperty("--columns", columns);
+        wrapper.style.setProperty("--rows", rows);
+        createTiles(columns * rows);
+    }
+
+    createGrid();
+
+    window.onresize = () => createGrid();
 }
 
 initAboutPage();
--- js/home.js
+++ js/home.js
@@ -1,52 +1,10 @@
-function initHomePage() {
-    const wrapper = document.getElementById("tiles");
+function initHomePage() {    
+    const container = document.querySelector("#blocks"),
+        tile = document.querySelector(".block");
 
-    let columns = 0, rows = 0, toggled = false;
-
-    // const toggle = () => {
-    //     toggled = !toggled;
-    //     document.querySelector("main[data-barba-namespace='home']").classList.toggle("toggled");
-    // }
-
-    // const handleOnClick = index => {
-    //     toggle();
-    //     anime({
-    //         targets: ".tile",
-    //         opacity: toggled ? 0 : 1,
-    //         delay: anime.stagger(50, {
-    //             grid: [columns, rows],
-    //             from: index
-    //         })
-    //     });
-    // }
-
-    const createTile = index => {
-        const tile = document.createElement("div");
-        tile.classList.add("tile");
-        // tile.style.opacity = toggled ? 0 : 1;
-        // tile.onclick = e => handleOnClick(index);
-        return tile;
+    for (let i = 0; i < 1599; i++) {
+        container.appendChild(tile.cloneNode());
     }
-
-    const createTiles = quantity => {
-        Array.from(Array(quantity)).map((tile, index) => {
-            wrapper.appendChild(createTile(index));
-        });
-    }
-
-    const createGrid = () => {
-        wrapper.innerHTML = "";
-        const size = document.body.clientWidth > 800 ? 100 : 50;
-        columns = Math.floor(document.body.clientWidth / size);
-        rows = Math.floor(document.body.clientHeight / size);
-        wrapper.style.setProperty("--columns", columns);
-        wrapper.style.setProperty("--rows", rows);
-        createTiles(columns * rows);
-    }
-
-    createGrid();
-
-    window.onresize = () => createGrid();
 
     let index = 0,
         interval = 1000;
